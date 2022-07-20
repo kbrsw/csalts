1d3457e18ee446d092414a56c3bd163c9e00205c
KostyaPC <kbroz82@gmail.com>
2022-07-20 11:51:23 +0300

    Summation example

--- src/main/java/com/example/calculus/HelloController.java
+++ src/main/java/com/example/calculus/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.calculus;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField Val1;
+    @FXML
+    private TextField Val2;
+    @FXML
+    private TextField Val3;
+
+
+    public void calc(ActionEvent event) {
+        String a = Val1.getText();
+        String b = Val2.getText();
+        double a1 = Double.parseDouble(a);
+        double b1 = Double.parseDouble(b);
+        double c1 = a1+b1;
+        Val3.setText(String.valueOf(c1));
     }
+
 }
